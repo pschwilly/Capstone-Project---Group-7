895f2efa801c3226075aa69db814ea3ff31aa8a1
pschwilly <pschwilly@gmail.com>
2026-04-08 17:48:53 +0000

    Clean up repo - remove upstream files, keep group work only

--- script/ti/nef_af_post.sh
+++
@@ -1,6 +0,0 @@
-set -e
-
-curl -X POST -H "Content-Type: application/json" --data @./nef_ti_anyUE.json \
-	http://nef.free5gc.org:8000/3gpp-traffic-influence/v1/af001/subscriptions
-
-exit 0
--- script/ti/udr_ti_del.sh
+++
@@ -1,6 +0,0 @@
-set -e
-
-curl -X DELETE -H "Content-Type: application/json" \
-	http://udr.free5gc.org:8000/nudr-dr/v2/application-data/influenceData/1
-
-exit 0
